decf0df9be0fa5f2c3dce3f76399eaa75f7cbbda
rcwoshimao <mantoumiaoshen@gmail.com>
2023-09-26 15:19:28 -0700

    First commit

--- ios101-lab1-mariokart-starter-master/README.md
+++ ios101-lab1-mariokart-starter-master/README.md
@@ -3,6 +3,3 @@
 ### Overview
 
 This is the starter project for iOS 101 Lab 1 Mario Kart
-
-![Video walkthrough](https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXRjeXN5dnV4ZHoxZmkxa3EybTk0dG01bGtubDR4cWF0NzFkODU3dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fz1xA5e3uQ9fzwQZUq/giphy.gif)
-
---
+++ ios101-lab3-cloudcast-starter-main/CloudCast/ForecastViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Rebecca Chen on 2023/9/26.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var forecastImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+        selectedForecastIndex = max(0, selectedForecastIndex - 1) // don't go lower than 0 index
+            configure(with: forecasts[selectedForecastIndex]) // change the forecast shown in the UI
+    }
+    
+    @IBAction func didTapFowardButton(_ sender: UIButton) {
+        selectedForecastIndex = min(forecasts.count-1, selectedForecastIndex + 1) // don't go lower than 0 index
+            configure(with: forecasts[selectedForecastIndex]) // change the forecast shown in the UI
+    }
+    
+    private var forecasts = [WeatherForecast]()
+    private var selectedForecastIndex = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        forecasts = createMockData()
+        configure(with: forecasts[selectedForecastIndex]) // configure the UI to show the first mock data
+        // Do any additional setup after loading the view.
+    }
+    
+    private func createMockData() -> [WeatherForecast]{
+        // This is just using the Calendar API to get a few random dates
+             let today = Date()
+             var dateComponents = DateComponents()
+             dateComponents.day = 1
+             let tomorrow = Calendar.current.date(byAdding: dateComponents, to: today)!
+             let dayAfterTomorrow = Calendar.current.date(byAdding: dateComponents, to: tomorrow)!
+             // Create a few mock data to show
+             let mockData1 = WeatherForecast(windSpeed: 3.3,
+                                             weatherCode: .violentRainShowers,
+                                             precipitation: 33.3,
+                                             temperature: 59.5,
+                                             date:today)
+             let mockData2 = WeatherForecast(windSpeed: 3.7,
+                                             weatherCode: .fog,
+                                             precipitation: 13.3,
+                                             temperature: 65.5,
+                                             date: tomorrow)
+             let mockData3 = WeatherForecast(windSpeed: 2.8,
+                                             weatherCode: .partlyCloudy,
+                                             precipitation: 23.3,
+                                             temperature: 49.5,
+                                             date: dayAfterTomorrow)
+             return [mockData1, mockData2, mockData3]
+    }
+
+    private func configure(with forecast: WeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)°F"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: forecast.date)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
